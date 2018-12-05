bb8dc7c4a8217b379e219d379241bfede8f86090
MaxFish1992 <348756718@qq.com>
2018-12-05 17:35:26 +0800

    Repository

---
+++ GisMng.IRepository/IBaseRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GisMng.IRepository
+{
+    public interface IBaseRepository
+    {
+    }
+}
---
+++ GisMng.Repository/BaseRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GisMng.Repository
+{
+    public class BaseRepository
+    {
+    }
+}
